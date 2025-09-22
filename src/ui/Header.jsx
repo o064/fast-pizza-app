794275a9dfdc1846132b070bfeb9a90d36f68092
omar <om039919@gmail.com>
2025-09-22 03:00:41 +0300

    fix userName component

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
 import SearchOrder from '../features/order/SearchOrder';
-import Username from '../features/user/Username';
+import UserName from '../features/user/UserName';
 
 function Header() {
   return (
@@ -10,7 +10,7 @@ function Header() {
       </Link>
 
       <SearchOrder />
-      <Username />
+      <UserName />
     </header>
   );
 }
